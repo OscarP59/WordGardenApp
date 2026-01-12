37173dd7ddd81f0c6ae13ec52d1a41b63f35f599
OscarP59 <oscarperla917@gmail.com>
2026-01-12 14:56:55 -0800

    Initial Commit

---
+++ WordGardenApp/WordGardenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenAppApp.swift
+//  WordGardenApp
+//
+//  Created by PERLA, OSCAR on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
