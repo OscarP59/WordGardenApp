9f3922eaaead93512d592d573b14d9859aacbd56
OscarP59 <oscarperla917@gmail.com>
2026-01-16 14:25:21 -0800

    made it so that when the person typs it automaticle gose to upercase and I also made it so that the user cant typ an emoji,sympol, or space

--- WordGardenApp/ContentView.swift
+++ WordGardenApp/ContentView.swift
@@ -8,14 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var wordsToGuess = ["SWIFT", "DOG", "CAT"]
+    @State private var gameStatusMessage = "How many Gusses to uncover the hidden word"
+    @State private var currentWord = 0 // index in
+    @State private var guessedLetter = ""
+    @State private var imageName  = "flower8"
+    @State private var playAgainHidden = true
+    @FocusState private  var textFeildIsFocused: Bool
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            HStack{
+                VStack(alignment: .leading){
+                    Text("words Gussed: \(wordsGuessed)")
+                    Text("words Missed: \(wordsMissed)")
+                }
+                Spacer()
+                VStack(alignment: .trailing){
+                    Text("words to Gussed: \(wordsToGuess.count - (wordsGuessed + wordsMissed))")
+                    Text("words in game:\(wordsToGuess.count)")
+                }
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+            Text(gameStatusMessage )
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            
+            Text("_ _ _ _ _")
+                .font(.title)
+            if playAgainHidden{
+                
+                HStack{
+                    TextField( "", text: $guessedLetter)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 30)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.gray, lineWidth: 2)
+                        }
+                        .keyboardType(.asciiCapable)
+                        .submitLabel(.done)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.characters)
+                        .onChange(of: guessedLetter) {
+                            guessedLetter =
+                            guessedLetter.trimmingCharacters(in: .letters.inverted)
+                            guard let lastChar = guessedLetter.last else{
+                                return
+                            }
+                            guessedLetter = String(lastChar).uppercased()
+                        }
+                        .focused($textFeildIsFocused)
+                    Button("Guesse a letter") {
+                        textFeildIsFocused = false
+//                        playAgainHidden = false
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(.mint)
+                    .disabled(guessedLetter.isEmpty)
+                }
+            }else{
+                Button("Another word"){
+//                    playAgainHidden = true
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            
+              
+            }
+            Spacer()
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .background(ignoresSafeAreaEdges: .bottom)
     }
 }
 
